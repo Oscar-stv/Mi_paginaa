c942d1df175a9a35d8c23af6d7858b3d374d8499
Oscar Reyes Sanchez <159722741+Oscar-stv@users.noreply.github.com>
2026-02-03 19:12:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Animar secciones al aparecer
 gsap.utils.toArray("section").forEach(sec=>{
 gsap.from(sec,{
 y:60,
 opacity:0,
-duration:.9,
+duration:.8,
 scrollTrigger:{
 trigger:sec,
 start:"top 85%",
 once:true
-}
-});
-});
-
-// Stack con profundidad ligera
-gsap.utils.toArray(".stack-card").forEach((card,i)=>{
-gsap.fromTo(card,
-{y:100,opacity:0,scale:.9},
-{
-y:0,
-opacity:1,
-scale:1 - i*.04,
-scrollTrigger:{
-trigger:card,
-start:"top 85%",
-scrub:true
 }
 });
 });
